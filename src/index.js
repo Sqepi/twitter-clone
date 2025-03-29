31564a7eb1c421bc284c760452e2d25cf4414155
Sqepi <your-email@example.com>
2025-03-29 22:29:03 +0100

    feat: add test CSS file and update index.js to include it

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import './test.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import './test.css'; // Make sure this line exists
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
